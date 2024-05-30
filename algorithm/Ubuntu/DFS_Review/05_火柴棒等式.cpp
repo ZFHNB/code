127c4bd9feac5dc0c0baab600dcef234a2b2d0a9
ZFHNB <2795337322@qq.com>
2024-05-30 16:21:17 +0800

    Ubuntu

---
+++ algorithm/Ubuntu/DFS_Review/05_火柴棒等式.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+const int N = 10010;
+
+int n;
+
+int arr[10];//枚举和的数组
+int hc[N] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};//火柴棍数量
+int res;
+int cal(int x)//计算数字对应所需的火柴数
+{
+    if(hc[x] > 0) return hc[x];
+    else
+    {
+        int sumFire = 0;
+        while(x)
+        {
+            sumFire += hc[x % 10];
+            x /= 10;
+        }
+        return sumFire;
+    }
+}
+
+void dfs(int x, int sum)
+{
+    if(sum > n) return;
+    if(x > 3)
+    {
+        if(arr[1] + arr[2] == arr[3] && sum == n)
+        {
+            res++;
+        }
+        return;
+    }
+    for(int i = 0; i <= 1000; i++)
+    {
+        arr[x] = i;
+        dfs(x + 1, sum + cal(i));
+    }
+}
+
+int main()
+{
+    scanf("%d", &n);
+    n -= 4;
+    dfs(1, 0);
+    printf("%d", res);
+
+    return 0;
+}
